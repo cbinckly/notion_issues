4f1dd79713f7095a9908370e4ce14d5c67f5923c
cbinckly <cbinckly@juniper.net>
2022-07-24 13:09:58 -0400

    get the jira basics working.

--- notion_issues/sources/jira.py
+++ notion_issues/sources/jira.py
@@ -1,22 +1,28 @@
 import jira
+from jira.exceptions import JIRAError
 
 from notion_issues import IssueSource, unassigned_user
+from notion_issues.logger import Logger
 
-DEFAULT_JIRA_SERVER = "https://jiracloud.net"
+log = Logger('notion_issues.sources.jira')
 
 class JiraSource(IssueSource):
 
     closed_statuses = ['closed', 'resolved']
 
-    def __init__(self, jira_token, jira_project,
-                 jira_server=DEFAULT_JIRA_SERVER):
-        self.jira = JIRA(options={'server': args.server},
-                         token_auth=args.jira_token)
+    def __init__(self, jira_token, jira_project, jira_server):
+        self.jira = jira.JIRA(options={'server': jira_server},
+                         token_auth=jira_token)
         self.project = jira_project
+
+    def map_unassigned_user(self, user):
+        if user == unassigned_user:
+            return None
+        return user
 
     def get_issues(self):
         output = {}
-        issues = jira.search_issues(f'project={self.project}')
+        issues = self.jira.search_issues(f'project={self.project}')
         for issue in issues:
             if issue.fields.assignee:
                 assignee = issue.fields.assignee.name
@@ -28,26 +34,45 @@ class JiraSource(IssueSource):
                   "status": issue.fields.status.name,
                   "assignee": assignee,
                   "reporter": issue.fields.creator.key,
-                  "labels": [issue.fields.priority,
+                  "labels": [issue.fields.priority.name,
                              issue.fields.issuetype.name],
                   "due_on": self.normalize_date(
-                                issue.fields.due_date, granularity='minutes'),
+                                issue.fields.duedate, granularity='minutes'),
                   "opened_on": self.normalize_date(
                                 issue.fields.created, granularity='minutes'),
                   "updated_on": self.normalize_date(issue.fields.updated),
-                  "link": issue.permalink
+                  "link": issue.permalink()
             }
 
         return output
 
     def update_issue(self, key, issue_dict):
-        issue = jira.issue(key)
-        issue.update(fields={
-                  "summary": issue_dict['title'],
-                  "status": issue_dict['status'],
-                  "assignee": { "name": issue_dict['assignee'] },
-                  "due_on": issue_dict['due_on']
-            })
+        issue = self.jira.issue(key)
+        fields = {
+                "summary": issue_dict['title'],
+                "assignee": {
+                  "name": self.map_unassigned_user(issue_dict['assignee'])
+                },
+            }
+        if issue_dict['due_on']:
+            fields["duedate"] = issue_dict['due_on']
+
+        try:
+            issue.update(fields=fields)
+            log.info(f"{key}: update to {fields}")
+        except JIRAError as e:
+            log.error(f"failed to update: {e}")
+
+        status = issue_dict['status']
+        for transition in self.jira.transitions(issue):
+            name = transition['to']['name']
+            log.debug(f'assessing transition to {name}=={status}?')
+            if name == status:
+                log.info(f'{key}: transitioning issue to {name}.')
+                try:
+                    self.jira.transition_issue(issue, transition['id'])
+                except JIRAError as e:
+                    log.error(f"failed to transition: {e}")
 
     def __str__(self):
         return f"Jira Source: {self.project}"

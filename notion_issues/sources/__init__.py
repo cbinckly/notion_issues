cb36b3cc08caa9f760804902485e0c6ec55ba2c1
cbinckly <cbinckly@gmail.com>
2022-07-30 10:04:25 -0400

    new home os issue source abstract class

---
+++ notion_issues/sources/__init__.py
@@ -0,0 +1,38 @@
+from dateutil import parser
+
+ISO_UTC_FMT = "%Y-%m-%dT%H:%M:%SZ"
+ISO_UTC_MIN_FMT = "%Y-%m-%dT%H:%M:00Z"
+
+class IssueSource():
+
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError("Implement in child.")
+
+    def normalize_date(self, date, granularity='seconds'):
+        if not date:
+            return ""
+        if isinstance(date, str):
+            date = parser.isoparse(date)
+        if granularity == 'seconds':
+            return date.strftime(ISO_UTC_FMT)
+        return date.strftime(ISO_UTC_MIN_FMT)
+
+    def get_issues(self):
+        """
+        :returns: issues dict
+        """
+        raise NotImplementedError("Implement in child.")
+
+    def update_issue(self, issue_key, issues_dict):
+        """
+        :param issue_key: unique issue key.
+        :type issue_key: str
+        :param issues_dict: issues dict containing issues to update.
+        :type issues_dict: dict
+        """
+        raise NotImplementedError("Implement in child.")
+
+    def __str__(self):
+        raise NotImplementedError("Implement in child.")
+
+
